d1bd01845296b7e5da60db1500c1cfe048dcc01b
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-13 14:14:26 +0300

    Creating a submit project form

--- awards/forms.py
+++ awards/forms.py
@@ -5,3 +5,9 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields=['profile_picture','bio','email','phone_number'] 
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields=['title','image','description','country','link']         
+ 
