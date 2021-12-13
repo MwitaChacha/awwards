e08175614bd7ba8eb985124bcac7d5f0e6d33103
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-13 14:10:54 +0300

    Creating the Project model

--- awards/models.py
+++ awards/models.py
@@ -12,5 +12,23 @@ class Profile(models.Model):
     phone_number = models.IntegerField(blank=True)
     
     def __str__(self):
-        return self.user  
+        return self.user 
+    
+class Project(models.Model):   
+    user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='project_user')
+    title = models.CharField(max_length=100)
+    image = CloudinaryField('image') 
+    description = HTMLField()
+    link = models.CharField(max_length=250)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=100, default="")
+  
+    @classmethod
+    def search_project(cls,search_term):
+        projects = cls.objects.filter(Q(user__username=search_term) | Q(title__icontains=search_term))
+        return projects  
+      
+    
+    def __str__(self):
+        return self.title        
     
