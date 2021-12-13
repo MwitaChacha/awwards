55025a49042448fbb70c8362dcb53da8701b3236
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-13 14:23:42 +0300

    Adding essential imports in views

--- awards/views.py
+++ awards/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Project, Profile
+from .forms import ProfileForm, ProjectForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def index(request):
