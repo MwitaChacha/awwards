b07965d37c5368367150a38f68c52559f386234b
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-13 14:25:33 +0300

    Updating the index view

--- awards/views.py
+++ awards/views.py
@@ -7,7 +7,19 @@ from django.contrib.auth.models import User
 
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
-    
-    return render(request, 'index.html')
+    current_user = request.user
+    try:
+        if not request.user.is_authenticated:
+            return redirect('/accounts/login/')
+        current_user = request.user
+        profile =Profile.objects.get(user=current_user)
+        
+    except ObjectDoesNotExist:
+        return redirect('update_profile')
+    profiles = Profile.objects.filter(user_id = current_user.id).all()
+    projects = Project.objects.all().order_by('-posted_at')
+    return render(request, 'index.html',{"profiles": profiles, "projects":projects})
+
     
