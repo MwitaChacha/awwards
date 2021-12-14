67ef048a44cfdb1156e14cb9c727304cff58fde3
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-14 17:18:45 +0300

    Updating requirements file

--- awwards/settings.py
+++ awwards/settings.py
@@ -22,7 +22,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+LOGIN_REDIRECT_URL='/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
