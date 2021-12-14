cf9e592e11fc04b737330d5c00d94bf539080c35
Mwita Chacha <jonahhchacha@gmail.com>
2021-12-14 08:32:12 +0300

    Creating API endpoints

--- awards/views.py
+++ awards/views.py
@@ -4,7 +4,9 @@ from django.core.exceptions import ObjectDoesNotExist
 from .models import Project, Profile, Review
 from .forms import ProfileForm, ProjectForm, RateForm
 from django.contrib.auth.models import User
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProjectSerializer, ProfileSerializer
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -118,3 +120,15 @@ def rate_project(request, id):
             rate.save()
             return redirect('index')
     
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)  
+    
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)      
